5492cacd3f791a51d1da7cfdda319b3aa78f0721
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2023-07-31 05:52:18 -0700

    do around up to complete these gsc pt3 4 opengl

---
+++ GSC_LIB_OpenWorld/04/GSC_0x030100010380046C_pt3.py
@@ -0,0 +1,19 @@
+from struct import unpack
+
+faces=[]
+#traingles
+
+def GSC_4_():
+    global VertexCount2_pt3
+    fa = 0
+    fb = 1
+    fc = 2
+    for i in range(VertexCount2_pt3-vertexCount2_pt3+1):
+        if VertexCount2_pt3 == 4:
+            fa+=1
+            fb+=1
+            fc+=1
+            faces.append([fa,fb,fc])
+            
+            
+                
