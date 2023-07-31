66d1a76f2ac6afd044acf68a8ca73fc0c6f2c420
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2023-07-31 02:12:02 -0700

    offset 4 triangle strips correct 0x0301000103046C

--- GSC_LIB_OpenWorld/04/GSC_0x030100010380046C.py
+++ GSC_LIB_OpenWorld/04/GSC_0x030100010380046C.py
@@ -1,10 +1,20 @@
 from struct import unpack
 
 faces=[]
-#triangles
+#trainglestrips
 
 def GSC_4_():
-    pass
+    global VertexCount2
+    fa = -1
+    fb = 0
+    fc = 1
+    faces = []
+    for i in range(VertexCount2-2):
+        if VertexCount2 == 4:
+            fa+=1
+            fb+=1
+            fc+=1
+            faces.append([fa,fb,fc])
             
             
                 
